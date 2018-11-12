edfa8b04c9a51601c3447f83a844635e57029362
Dominic <bug59438@hotmail.com>
2018-11-12 13:06:32 -0500

    Update finalproject.ino

--- finalproject.ino
+++ finalproject.ino
@@ -1,9 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 22;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  
 }
